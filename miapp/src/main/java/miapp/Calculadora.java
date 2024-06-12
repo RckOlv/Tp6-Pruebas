f9f4b95352e06e7e3806604842812e831af225eb
RckOlv <yosoyricky9@gmail.com>
2024-06-12 18:28:30 -0300

    Arreglos

--- miapp/src/main/java/miapp/Calculadora.java
+++ miapp/src/main/java/miapp/Calculadora.java
@@ -12,4 +12,16 @@ public class Calculadora {
     public static int cuadradoBinomio(int a, int b) {
         return a * a + 2 * a * b + b * b;
     }
+
+    public static void main(String[] args) {
+        // Aquí puedes llamar a tus métodos para probarlos
+        int resultadoSuma = suma(5, 3);
+        System.out.println("Suma: " + resultadoSuma);
+
+        int resultadoResta = resta(5, 3);
+        System.out.println("Resta: " + resultadoResta);
+
+        int resultadoCuadradoBinomio = cuadradoBinomio(2, 3);
+        System.out.println("Cuadrado del binomio: " + resultadoCuadradoBinomio);
+    }
 }
